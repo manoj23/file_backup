d00f829bf78399a1d4aac485496f524a6de19939
Georges Savoundararadj <savoundg@gmail.com>
2018-03-14 13:07:35 +0100

    Use inotifywait to commit on file change

--- file_backup.sh
+++ file_backup.sh
@@ -10,9 +10,11 @@ file_backup()
 	[ ! -f "$file_path" ] && echo "$file_path file does not exist" && exit 1
 	[ ! -d "$git_repo_path" ] && echo "$git_repo_path directory does not exist" && exit 2
 	[ ! -d "$git_repo_path/.git" ] && echo "$git_repo_path directory is not a git repository" && exit 3
+	[ ! which inotifywait > /dev/null 2>&1 ] && echo "inotifywait is not installed" && exit 4
 
-	cp -p $file_path $git_repo_path
-	cd $git_repo_path
-	git add $file_name && git commit -m "$file_commit_message"
-	cd -
+	inotifywait -q -m -e close_write --format %e $file_path |
+	while read events; do
+		cp -p $file_path $git_repo_path
+		(cd $git_repo_path && git add $file_name && git commit -m "$file_commit_message")
+	done
 }
