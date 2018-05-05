995cb69c5a6b24a95773e41cdba41ec67dd4b71f
Georges Savoundararadj <savoundg@gmail.com>
2018-05-05 14:27:01 -0700

    shellcheck: Fix SC2086: Double quote to prevent globbing and word splitting

--- file_backup.sh
+++ file_backup.sh
@@ -2,12 +2,12 @@
 
 monitor_file()
 {
-	inotifywait -q -m -e attrib,delete_self,close_write --format %e $file_path |
+	inotifywait -q -m -e attrib,delete_self,close_write --format %e "$file_path" |
 	while read events; do
-		echo $events
-		cp -p $file_path $git_repo_path
-		(cd $git_repo_path && \
-			git add $file_name && \
+		echo "$events"
+		cp -p "$file_path" "$git_repo_path"
+		(cd "$git_repo_path" && \
+			git add "$file_name" && \
 			git commit -m "$file_name: $(date --iso-8601)" && \
 			[ "x$is_remote_present" == "xyes" ] && git push)
 
@@ -22,7 +22,7 @@ file_backup()
 {
 	file_path=$1
 	git_repo_path=$2
-	file_name=$(basename $file_path)
+	file_name=$(basename "$file_path")
 	is_remote_present="no" # by default: we consider there is no remote
 
 	[ ! -f "$file_path" ] && echo "$file_path file does not exist" && exit 1
@@ -30,7 +30,7 @@ file_backup()
 	[ ! -d "$git_repo_path/.git" ] && echo "$git_repo_path directory is not a git repository" && exit 3
 	[ ! which inotifywait > /dev/null 2>&1 ] && echo "inotifywait is not installed" && exit 4
 
-	if (cd $git_repo_path && git ls-remote --exit-code origin > /dev/null 2>&1); then
+	if (cd "$git_repo_path" && git ls-remote --exit-code origin > /dev/null 2>&1); then
 		is_remote_present="yes"
 	fi
 
