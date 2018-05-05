796f3f73b844acf42ef318a72e17fa02842762ca
Georges Savoundararadj <savoundg@gmail.com>
2018-05-05 14:30:03 -0700

    shellcheck: Fix SC1072: Expected comparison operator (don't wrap commands in []/[[]])

--- file_backup.sh
+++ file_backup.sh
@@ -28,7 +28,7 @@ file_backup()
 	[ ! -f "$file_path" ] && echo "$file_path file does not exist" && exit 1
 	[ ! -d "$git_repo_path" ] && echo "$git_repo_path directory does not exist" && exit 2
 	[ ! -d "$git_repo_path/.git" ] && echo "$git_repo_path directory is not a git repository" && exit 3
-	[ ! which inotifywait > /dev/null 2>&1 ] && echo "inotifywait is not installed" && exit 4
+	! (which inotifywait > /dev/null 2>&1) && echo "inotifywait is not installed" && exit 4
 
 	if (cd "$git_repo_path" && git ls-remote --exit-code origin > /dev/null 2>&1); then
 		is_remote_present="yes"
